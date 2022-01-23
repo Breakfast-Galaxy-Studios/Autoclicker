96162dd030b691f8513c34ec23ec2e9b2a0fe863
gwerrry <tunstullcarlos@gmail.com>
2022-01-22 22:07:22 -0500

    Changes by Malcolm

--- main/src/main/java/net/breakfaststudios/BreakfastClicker.java
+++ main/src/main/java/net/breakfaststudios/BreakfastClicker.java
@@ -2,6 +2,6 @@ package net.breakfaststudios;
 
 public class BreakfastClicker {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("ooga booga");
     }
 }
